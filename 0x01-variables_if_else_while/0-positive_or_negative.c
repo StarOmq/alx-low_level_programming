978c20373e3977ca45b48b0839e123c0da521470
StarOmq <emmanuelwasonga01@gmail.com>
2023-02-19 03:34:33 -0800

    4-puts.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,10 +7,8 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always o (success)
  */
-
-
 int main(void)
 {
 	int n;
@@ -21,8 +19,8 @@ int main(void)
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n == 0)
-	        printf("%d is zero\n", n);
-        else (n < 0)
-	      printf("%d is negative\n", n);	
+		printf("%d is zero\n", n);
+	else(n < 0)
+		printf("%d is negative\n", n);
 	return (0);
 }
